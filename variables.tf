671b9215545d4580b852e4dc131654cd7a8c153f
nixao70 <nixao70@gmail.com>
2024-10-18 16:08:09 +0300

    add all

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "yandex_cloud_id" {
+  default = "b1gah11c321co65l2apf"
+}
+
+variable "yandex_folder_id" {
+  default = "b1g73kf911qh20t72eeb"
+}
+
+variable "b-zone" {
+  default = "ru-central1-b"
+}
+
+variable "lamp-instance-image-id" {
+  default = "fd827b91d99psvq5fjit"
+}
